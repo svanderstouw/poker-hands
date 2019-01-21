302bd61dca84cfba81eb07ada275506d38f6eed3
Steve Vander Stouw <svanderstouw@gmail.com>
2019-01-21 15:31:34 -0500

    Added main app and readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Poker Hands
+
+This project compares several pairs of poker hands and indicates which, if either, has a higher rank.
--- src/main/java/poker/PokerHandsApp.java
+++ src/main/java/poker/PokerHandsApp.java
@@ -4,13 +4,18 @@ public class PokerHandsApp {
 
 	public static void main(String[] args) {
 		
-		compareHands("Black: 2H 3D 5S 9C KD,  White: 2C 3H 4S 8C AH");
-
+		compareHands("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH");
+		compareHands("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S");
+		compareHands("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH");
+		compareHands("Black: TH AH 2H 4H JH  White: QS 2H QD QH QC");
+		compareHands("Black: 2D KD 3D QD TD  White: QD 9H JS TD 8C");
+		compareHands("Black: 2S 7S 7C QH 2D  White: 4H 2D 5C AH 3S");
+		
 	}
 	
 	public static void compareHands(String initialString) {
 		
-		String[] hands= initialString.split(",\\s");
+		String[] hands= initialString.split("\\s\\s");
 		Hand hand1 = new Hand(hands[0]);
 		Hand hand2 = new Hand(hands[1]);
 		
